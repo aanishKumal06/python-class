e8d7ced5db901d55b9d902548c7035f47373f92b
aanishKumal06 <anishkumal0202@gmail.com>
2025-06-20 09:41:12 +0545

    python basics

---
+++ 001_basics/main.py
@@ -0,0 +1,19 @@
+print('hello world')
+
+name = input("Enter your name: ")
+location = input("Enter your location: ")
+phone_number = input("Enter your phone number: ")
+age = input("Enter your age: ")
+qualification = input("Enter your qualification: ")
+
+print(f""" Hello {name}.
+Location: {location},
+Age: {age},
+Phone_number: {phone_number},
+Qualification: {qualification}
+Welcome to swift academy""")
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter seconf number: "))
+
+print(f" The sum of {num1} and {num2} is {num1 + num2}")
