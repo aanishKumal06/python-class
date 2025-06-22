525c80c6f7b41cbedaa0e05d393102daec54414a
aanishKumal06 <anishkumal0202@gmail.com>
2025-06-22 10:57:01 +0545

    File Handling

--- 008_file_handling/f03_reading_json_file.py
+++ 008_file_handling/f03_reading_json_file.py
@@ -5,3 +5,12 @@ file_path = r"C:\Users\anish\OneDrive\Desktop\python-class\.vscode\extensions.js
 with open(file_path, "r") as fp:
     content = json.load(fp)
     print(content["recommendations"][0])
+
+
+personal_details = '{"name": "Anish", "age": 18, "city": "Pokhara"}'
+
+convert_to_dist = json.loads(personal_details)
+
+
+for key, value in convert_to_dist.items():
+    print("Key: ", key, "Value: ", value)
