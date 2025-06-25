a4d5acb9dff3aef2a1161926f148ef9c871eedbf
aanishKumal06 <anishkumal0202@gmail.com>
2025-06-25 17:37:56 +0545

    Datatypes

---
+++ 002_datatypes/d09_list_comprehension.py
@@ -0,0 +1,8 @@
+nums = [1, 2, 3, 4, 5]
+
+even_nums = [num for num in nums if num % 2 == 0]
+
+square_nums = [num**2 for num in nums]
+
+print(f"Even Numbers Lists : {even_nums}")
+print(f"Square Numbers Lists : {square_nums}")
---
+++ 002_datatypes/d10_dictionary_comprehension.py
@@ -0,0 +1,7 @@
+nums = [1, 2, 3, 4, 5]
+
+new_dict = {
+    num: num + 100 for num in nums
+}
+
+print(new_dict)
