8d353c9aee5f09d90d58b656fe6ab98868868ca7
aanishKumal06 <anishkumal0202@gmail.com>
2025-06-29 10:37:25 +0545

    Modules

---
+++ 012_modules.py/calculator.py
@@ -0,0 +1,15 @@
+def add(a, b):
+    return a + b
+
+
+def sub(a, b):
+    return a - b
+
+
+def multiple(a, b):
+    return a * b
+
+
+if __name__ == "__main__":
+    print("Name for  module: ", __name__)
+    print(add(12, 3))
---
+++ 012_modules.py/main.py
@@ -0,0 +1,7 @@
+import calculator
+
+if __name__ == "__main__":
+    print(__doc__)
+    print(__file__)
+    print("Name for runner: ", __name__)
+    print(calculator.add(12, 3))
