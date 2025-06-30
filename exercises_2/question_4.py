8de60f042160ad0dbf64451b7cfcfe228810a458
aanishKumal06 <anishkumal0202@gmail.com>
2025-06-30 16:19:50 +0545

    Exercises

--- exercises_2/question_4.py
+++ exercises_2/question_4.py
@@ -1,12 +1,10 @@
-count = 0
 while True:
     try:
         number = int(input("ENter a number between 1 and 10: "))
         if 1 <= number <= 10:
-            print("Great choice!")
+            print(f"Great choice! {number}")
             break
         else:
-            count += 1
-            print(f"That's not between 1 and 10! Try again: {count}")
+            print(f"That's not between 1 and 10! Try again: {number}")
     except ValueError:
         print("Invalid input! Try Again!")
